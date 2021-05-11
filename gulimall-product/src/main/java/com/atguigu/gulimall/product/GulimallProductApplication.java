b10b140823805dbf12c90e7f3d181e4fab20e545
Dengyuesheng <705501518@qq.com>
2021-05-11 10:39:43 +0800

    reverse engineering for all modules

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,22 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 整合mybatis-plus：
+ *  1.导入mybatis-plus依赖
+ *  2.配置
+ *      1）配置数据源
+ *          导入数据库驱动
+ *          在application.yml文件中配置数据源信息
+ *      2）配置mybatis-plus
+ *          使用@MapperScan，指定mapper文件在哪
+ *          在application.yml中指定sql映射文件在哪
+ *
+ */
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
